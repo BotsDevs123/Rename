e4a4656ac2f92342f6fa873de4c0db903c5cb9bb
BotsDevs123 <93205572+BotsDevs123@users.noreply.github.com>
2021-12-05 15:32:22 +0530

    Update auto_caption.py

--- mt_privateautocaption/auto_caption.py
+++ mt_privateautocaption/auto_caption.py
@@ -7,10 +7,14 @@ from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from pyrogram.errors import FloodWait
 
+
+CAP = """[𝗬𝗢𝗨 𝗛𝗔𝗩𝗘 𝗡𝗢𝗧 𝗦𝗨𝗕𝗦𝗖𝗥𝗜𝗕𝗘𝗗 𝗧𝗢 𝗠𝗬 𝗬𝗢𝗨𝗧𝗨𝗕𝗘 𝗖𝗛𝗔𝗡𝗡𝗘𝗟. 𝗚𝗢 𝗤𝗨𝗜𝗖𝗞𝗟𝗬 𝗔𝗡𝗗 𝗦𝗨𝗕𝗦𝗖𝗥𝗜𝗕𝗘](https://youtube.com/c/MoTech_YT)"""
+
+
 @Client.on_message(filters.media & filters.channel)
 async def caption(client, message: Message):
     kopp, _ = get_file_id(message)
-    await message.edit(f"<b>[{kopp.file_name}](https://youtube.com/c/MoTech_YT)</b>")
+    await message.edit(f"<b>{kopp.file_name}</b>\n\n{CAP}")
 
 def get_file_id(msg: Message):
     if msg.media:
